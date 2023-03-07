40bb4b9670faa9a8d3069e6c5b99d3c8d3b07f61
ufuk murat sınavu <u.murat0606@hotmail.com>
2023-03-07 18:23:08 +0300

    addRepositoryInterface

---
+++ DataAccessLayer/Abstract/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetList();
+        T GetByID(int id);
+
+        List<T> GetAll(Expression<Func<T, bool>> filter);
+    }
+}
